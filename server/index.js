99ca6651f0f8ef270e1e516930cc48ecc318791c
SanjeedaVempalli <sanjeeda.vempathi@metlife.com>
2018-11-14 12:30:06 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app= express();
 const mongo=require("./dbConfig");
+const cors=require("cors");
 const getWorkspaceDetails=require("./routes/getWorkspaceDetails");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/',getWorkspaceDetails);
